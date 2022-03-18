d8fc86307a6a06ab40bfd77ffab7028422bb923a
hasuto <96404939+hasuto@users.noreply.github.com>
2022-03-18 10:05:12 +0900

    kousin

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 
 
-var title = document.getElementById("title");
-
-title = "hasuto";
-
 const FADEIN_ELEM = document.getElementById('fadein');
 const FADEIN_ELEM2 = document.getElementById('fadein2');
 const FADEIN_ELEM3 = document.getElementById('fadein3');
@@ -24,13 +20,10 @@ window.addEventListener('scroll', () => {
     }
 });
 
-//スクロール時に要素をフェードインさせるイベントを設定する
+
 window.addEventListener('scroll', () => {
-    //フェードインする要素の上部の位置を取得する
     const FADEIN_ELEM_TOP2 = FADEIN_ELEM2.getBoundingClientRect().top;
-    //ブラウザの高さを取得
     const WINDOW_HEIGHT2 = window.innerHeight;
-    //画面に表示された時にフェードインさせる処理
     if (WINDOW_HEIGHT2 > (FADEIN_ELEM_TOP2 + 200)) {
         FADEIN_ELEM2.classList.add('fadein-after');
     } else {
@@ -39,11 +32,8 @@ window.addEventListener('scroll', () => {
 });
 
 window.addEventListener('scroll', () => {
-    //フェードインする要素の上部の位置を取得する
     const FADEIN_ELEM_TOP3 = FADEIN_ELEM3.getBoundingClientRect().top;
-    //ブラウザの高さを取得
     const WINDOW_HEIGHT3 = window.innerHeight;
-    //画面に表示された時にフェードインさせる処理
     if (WINDOW_HEIGHT3 > (FADEIN_ELEM_TOP3 + 200)) {
         FADEIN_ELEM3.classList.add('fadein-after');
     } else {
@@ -52,11 +42,8 @@ window.addEventListener('scroll', () => {
 });
 
 window.addEventListener('scroll', () => {
-    //フェードインする要素の上部の位置を取得する
     const FADEIN_ELEM_TOP4 = FADEIN_ELEM4.getBoundingClientRect().top;
-    //ブラウザの高さを取得
     const WINDOW_HEIGHT4 = window.innerHeight;
-    //画面に表示された時にフェードインさせる処理
     if (WINDOW_HEIGHT4 > (FADEIN_ELEM_TOP4 + 200)) {
         FADEIN_ELEM4.classList.add('fadein-after');
     } else {
@@ -65,11 +52,8 @@ window.addEventListener('scroll', () => {
 });
 
 window.addEventListener('scroll', () => {
-    //フェードインする要素の上部の位置を取得する
     const FADEIN_ELEM_TOP5 = FADEIN_ELEM5.getBoundingClientRect().top;
-    //ブラウザの高さを取得
     const WINDOW_HEIGHT5 = window.innerHeight;
-    //画面に表示された時にフェードインさせる処理
     if (WINDOW_HEIGHT5 > (FADEIN_ELEM_TOP5 + 200)) {
         FADEIN_ELEM5.classList.add('fadein-after');
     } else {
@@ -78,11 +62,8 @@ window.addEventListener('scroll', () => {
 });
 
 window.addEventListener('scroll', () => {
-    //フェードインする要素の上部の位置を取得する
     const FADEIN_ELEM_TOP6 = FADEIN_ELEM6.getBoundingClientRect().top;
-    //ブラウザの高さを取得
     const WINDOW_HEIGHT6 = window.innerHeight;
-    //画面に表示された時にフェードインさせる処理
     if (WINDOW_HEIGHT6 > (FADEIN_ELEM_TOP6 + 200)) {
         FADEIN_ELEM6.classList.add('fadein-after');
     } else {
